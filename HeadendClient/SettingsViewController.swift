2b2a019ced91572106d7663e16508179ecf6afd1
Kin-Wai Koo <glug71@gmail.com>
2019-01-31 10:40:08 +0800

    Manual refresh button

--- HeadendClient/SettingsViewController.swift
+++ HeadendClient/SettingsViewController.swift
@@ -10,16 +10,27 @@ import TVUIKit
 class SettingsViewController: UIViewController {
     @IBOutlet weak var address: UITextField!
     @IBOutlet weak var port: UITextField!
+    @IBOutlet weak var refreshButton: UIButton!
+    
     
     let titleViewControllerIndex = 0
     let defaults = UserDefaults.standard
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+    override func viewDidLoad() {
+        super.viewDidLoad()
         address.text = defaults.object(forKey: "serveraddress") as? String ?? ""
         var p = defaults.integer(forKey: "serverport")
         if p == 0 { p = 9981 }
         port.text = String(p)
+        
+        if let buttonText = refreshButton.currentTitle {
+            refreshButton.setTitle(buttonText + " ✅", for: .disabled)
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        refreshButton.isEnabled = true
     }
     
     @IBAction func addressChanged(_ sender: Any) {
@@ -34,11 +45,16 @@ class SettingsViewController: UIViewController {
         refreshData()
     }
     
+    @IBAction func refreshServerData(_ sender: Any) {
+        refreshButton.isEnabled = false
+        setNeedsFocusUpdate()
+        updateFocusIfNeeded()
+        refreshData()
+    }
     private func refreshData() {
         guard let vc = self.tabBarController?.viewControllers else { return }
         if vc.count == 0 { return }
         guard let tvc = vc[0] as? TitleViewController else { return }
         tvc.refreshData = true
-        print("refresh!!!")
     }
 }
